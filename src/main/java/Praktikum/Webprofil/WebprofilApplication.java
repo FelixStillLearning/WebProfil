2c663fbad3c5e601b66a390f127e549c4b0558d0
FelixStillLearning <felixangga077@gmail.com>
2025-05-10 16:54:58 +0700

    Project Mentah

---
+++ src/main/java/Praktikum/Webprofil/WebprofilApplication.java
@@ -0,0 +1,13 @@
+package Praktikum.Webprofil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprofilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebprofilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Praktikum/Webprofil/WebprofilApplicationTests.java
@@ -0,0 +1,13 @@
+package Praktikum.Webprofil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprofilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
