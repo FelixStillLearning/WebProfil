4a51c02c1d7657c2797d5fb8f029eeb380786a94
Felix Angga Resky <151905402+FelixStillLearning@users.noreply.github.com>
2025-05-21 18:32:20 +0700

    nambah read update dan delete

--- src/main/java/Praktikum/Webprofil/controller/BukuController.java
+++ src/main/java/Praktikum/Webprofil/controller/BukuController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,6 +38,28 @@ public class BukuController {
         return "redirect:/buku";
     }
 
-
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        model.addAttribute("buku", bukuService.findById(id));
+        return "buku/form";
+    }
+    
+    @PostMapping("/update")
+    public String update(@ModelAttribute Buku buku) {
+        bukuService.save(buku);
+        return "redirect:/buku";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        bukuService.deleteById(id);
+        return "redirect:/buku";
+    }
+    
+    @GetMapping("/view/{id}")
+    public String view(@PathVariable Long id, Model model) {
+        model.addAttribute("buku", bukuService.findById(id));
+        return "buku/view";
+    }
     
 }
--- src/main/java/Praktikum/Webprofil/service/BukuService.java
+++ src/main/java/Praktikum/Webprofil/service/BukuService.java
@@ -1,6 +1,7 @@
 package Praktikum.Webprofil.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,17 @@ public class BukuService {
     public List<Buku> findAll() {
         return bukuRepository.findAll();
     }
+    
     public Buku save(Buku buku) {
         return bukuRepository.save(buku);
     }
     
+    public Buku findById(Long id) {
+        Optional<Buku> result = bukuRepository.findById(id);
+        return result.orElse(null);
+    }
+    
+    public void deleteById(Long id) {
+        bukuRepository.deleteById(id);
+    }
 }
